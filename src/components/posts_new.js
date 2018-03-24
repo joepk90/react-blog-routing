4bebc2a77ebe48b2b49186a0240eebb9cab47609
Joe Park-Kennaby <jparkkennaby@gmail.com>
2018-03-24 15:08:46 +0000

    refactor posts_new - improve code by removing repeated form fields markup - use function instead

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,7 +1,25 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
+
+const FIELDS = {
+  title: {
+    type: 'input',
+    label: 'Title for Post'
+  },
+  categories: {
+    type: 'input',
+    label: 'Enter some categories for this post'
+  },
+  content: {
+    type: 'textarea',
+    label: 'Post Contents'
+  },
+};
+
+// ['title', 'categories', 'content'];
 
 class PostsNew extends Component {
 
@@ -20,8 +38,22 @@ class PostsNew extends Component {
       });
   }
 
+renderField(fieldConfig, field) {
+  const fieldHelper = this.props.fields[field];
+
+  return (
+    <div className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'has-danger' : '' }`}>
+      <label>{fieldConfig.label}</label>
+      <fieldConfig.type type="text" className="form-control" {...fieldHelper}/>
+      <div className="text-help">
+      {fieldHelper.touched ? fieldHelper.error : ''}
+      </div>
+    </div>
+  );
+}
+
   render() {
-    const { fields: { title, categories, content }, handleSubmit} = this.props;
+    const { handleSubmit} = this.props;
     // const { handleSubmit} = this.props; This assignment is the same as the above line below
     // (the assigned variable 'handleSubmit' must be the same value being assigned).
     // const handleSubmit = this.props.handleSubmit;
@@ -30,29 +62,8 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
-        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : '' }`}>
-          <label>Title</label>
-          <input type="text" className="form-control" {...title}/>
-          <div className="text-help">
-          {title.touched ? title.error : ''}
-          </div>
-        </div>
 
-        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : '' }`}>
-          <label>Categories</label>
-          <input type="text" className="form-control" {...categories}/>
-          <div className="text-help">
-          {categories.touched ? categories.error : ''}
-          </div>
-        </div>
-
-        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : '' }`}>
-          <label>Content</label>
-          <textarea type="text" className="form-control" {...content}/>
-          <div className="text-help">
-          {content.touched ? content.error : ''}
-          </div>
-        </div>
+      {_.map(FIELDS, this.renderField.bind(this))}
 
         <button type="submit" className="btn btn-primary">Submit</button>
         <Link to="/" className="btn btn-danger">Cancel</Link>
@@ -65,15 +76,12 @@ class PostsNew extends Component {
 function validate(values) {
   const errors = {};
 
-if (!values.title) {
-  errors.title = 'Enter a title';
-}
-if (!values.categories) {
-  errors.categories = 'Enter categories';
-}
-if (!values.content) {
-  errors.content = 'Enter some content';
-}
+
+_.each(FIELDS, (type, field) => {
+  if(!values[field]) {
+    errors[field] = `Enter a ${field}`;
+  }
+});
 
   return errors;
 }
@@ -83,6 +91,6 @@ if (!values.content) {
 
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content'],
+    fields: _.keys(FIELDS),
     validate
 }, null, {createPost} )(PostsNew);
