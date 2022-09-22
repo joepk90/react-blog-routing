c68ff8a906f056296d411a2cbfcfe45eff291ea9
Alex <43220424+AlexvEngelen@users.noreply.github.com>
2022-09-22 13:12:14 +0200

    Deleted working API key
    You dont want to store a public API key in the code, you want this key stored in the .env file.
    If this was a big application with data that cant be leaked then this will be a big problem.

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_POST = 'FETCH_POST';
 export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=sgjvwojsggowrgoworg';
+const API_KEY = '?key=<KEY>';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
